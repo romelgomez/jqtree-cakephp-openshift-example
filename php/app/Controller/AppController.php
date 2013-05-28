e2f250cf145fa7ad2dae97ec52596baf18e30bfe
Timothy M. Crider <timcrider@gmail.com>
2013-05-28 08:36:20 +0000

    Adding in hooks to activate DebugKit plugin.
    Commented out the default cakePHP sql viewer as it is now handled by DebugKit.

--- php/app/Controller/AppController.php
+++ php/app/Controller/AppController.php
@@ -32,4 +32,8 @@ App::uses('Controller', 'Controller');
  * @link http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+	/**
+	 * Add in the DebugKit toolbar
+	 */
+	public $components = array('DebugKit.Toolbar');
 }
